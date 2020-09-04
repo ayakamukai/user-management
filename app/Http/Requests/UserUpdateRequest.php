c265af591f600c2d830f86effeebe48945012385
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-09-04 16:51:39 +0900

    [課題１]詳細と更新ページとバリエーションを追加
    ・更新はフォームリクエストバリエーションでUserUpdateRequestに

---
+++ app/Http/Requests/UserUpdateRequest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UserUpdateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'name'=> ['required','max:255'],
+            'login_id'=> ['required','max:255', Rule::unique('users')->ignore($this->id)],
+            'email'=> ['required','max:255','email'],
+            'password'=> ['nullable','between:8,32','regex:/^[a-zA-Z-_]+$/']
+        ];
+    }
+
+    public function messages()
+    {
+        return [
+            'required' => ':attributeが入力されていません',
+            'between' => 'パスワードは8～32文字で入力してください',
+            'login_id.unique' => 'ログインIDが重複しています',
+            'email' => '正しい形式で入力してください',
+            'password.regex' => '半角英字または半角ハイフンのみ使用可能です'
+            ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'name' => '名前',
+            'login_id' => 'ログインID',
+            'email' => 'メールアドレス',
+            'password' => 'パスワード'];
+    }
+}
---
+++ resources/views/user/edit.blade.php
@@ -0,0 +1,81 @@
+@extends('user.template')
+@section('title', 'ユーザー管理 - 編集')
+@section('content')
+
+<div class="container">
+  <div class="m-2 p-3 bg-white">
+    <h4 class="mb-5">ユーザー編集</h4>
+
+    <div class="offset-9 col-2">
+      <a href="{{ route('index') }}">一覧に戻る</a>
+    </div>
+
+    @if ($errors->any())
+      <div class="alert alert-danger">エラーがありました！</div>
+    @endif
+
+    <div class="inner-container"> 
+      <div class="m-2 p-3 bg-white">
+      <form action="{{ route('update', $user->id) }}" method="post" enctype="multipart/form-data">
+      {{ csrf_field() }}
+      {{ method_field('PUT') }}
+
+      <div class="form-group row">
+        <label class="offset-1 col-2">名前</label>
+        <div class="col-6">
+          <input type="text" class="form-control @if ($errors->has('name')) is-invalid @endif" name="name" value="{{ old('name', $user->name) }}">
+          @if ($errors->has('name'))
+          <div class="invalid-feedback">
+            {{ $errors->first('name') }}
+          </div>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label class="offset-1 col-2">ログインID</label>
+        <div class="col-6">
+          <input type="text" class="form-control @if ($errors->has('login_id')) is-invalid @endif" name="login_id" value="{{ old('login_id', $user->login_id) }}">
+          @if ($errors->has('login_id'))
+          <div class="invalid-feedback">
+            {{ $errors->first('login_id') }}
+          </div>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label class="offset-1 col-2">メールアドレス</label>
+        <div class="col-6">
+          <input type="text" class="form-control @if ($errors->has('email')) is-invalid @endif" name="email" value="{{ old('email', $user->email) }}">
+          @if ($errors->has('email'))
+          <div class="invalid-feedback">
+            {{ $errors->first('email') }}
+          </div>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group row">
+        <label class="offset-1 col-2">パスワード</label>
+        <div class="col-6">
+          <input type="password" class="form-control @if ($errors->has('password')) is-invalid @endif" name="password" value="{{ old('password') }}">
+          @if ($errors->has('password'))
+          <div class="invalid-feedback">
+            {{ $errors->first('password') }}
+          </div>
+          @endif
+        </div>
+      </div>
+
+      <div class="form-group row m-5">
+       <button class="btn btn-primary offset-4 col-2" type="submit">登録する</button>
+      </div>
+    </form>
+
+    </div>
+  </div>
+  
+  </div>
+</div>
+@endsection
