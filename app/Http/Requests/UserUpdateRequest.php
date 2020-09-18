dec107b76b8a9bd55a0d233e0d1f8fb60cfcff35
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-09-19 01:17:32 +0900

    [課題２]追加項目のリクエストバリデーションを追加

--- app/Http/Requests/UserUpdateRequest.php
+++ app/Http/Requests/UserUpdateRequest.php
@@ -28,7 +28,12 @@ class UserUpdateRequest extends FormRequest
             'name'=> ['required','max:255'],
             'login_id'=> ['required','max:255', Rule::unique('users')->ignore($this->id)],
             'email'=> ['required','email','max:255', Rule::unique('users')->ignore($this->id)],
-            'password'=> ['nullable','between:8,32','regex:/^[a-zA-Z-_]+$/']
+            'password'=> ['nullable','between:8,32','regex:/^[a-zA-Z-_]+$/'],
+            'sex'=> ['in:male,female'],
+            'zip'=> ['regex:/^\d{3}[-]\d{4}$|^\d{7}$/','nullable'],
+            'prefercture'=> ['Rule::in(config"pref")'],
+            'address'=> ['max:255'],
+            'note'=> ['max:2000']
         ];
     }
 
@@ -41,7 +46,10 @@ class UserUpdateRequest extends FormRequest
             'email.unique' => ':attributeが重複しています。変更して下さい',
             'email.email' => '正しい:attribute形式で入力して下さい。',
             'max' => ':max文字以内で入力して下さい',
-            'password.regex' => '半角英字、半角ハイフンまたは半角アンダースコアで入力して下さい'
+            'password.regex' => '半角英字、半角ハイフンまたは半角アンダースコアで入力して下さい',
+            'sex.in' => '男・女から選択して下さい',
+            'zip.regex' => '7桁、または半角ハイフンを含む3桁-4桁の形式で入力して下さい',
+            'prefecture' => '都道府県から選択して下さい',
             ];
     }
 
@@ -51,6 +59,11 @@ class UserUpdateRequest extends FormRequest
             'name' => '名前',
             'login_id' => 'ログインID',
             'email' => 'メールアドレス',
-            'password' => 'パスワード'];
+            'password' => 'パスワード',
+            'sex' => '性別',
+            'zip' => '郵便番号',
+            'prefecture' => '都道府県',
+            'address' => '住所',
+            'note' => '備考'];
     }
 }
