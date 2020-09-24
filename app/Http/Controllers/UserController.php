5a7f9b637fd26c9cf3e33bf4c9919017f43eeedf
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-09-24 22:07:43 +0900

    [課題３]検索フォームとクエリ作成

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,10 +10,45 @@ use App\Models\User;
 
 class UserController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        $users = User::paginate(10);
-        return view('user/index', ['users' => $users]);
+    //条件キー
+    $name_key = $request->input('name_key'); 
+    $id_key = $request->input('id_key');
+    $sex_key = $request->input('sex_key');
+    $pref_key = $request->input('pref_key');
+    $from_key = $request->input('from_key');
+    $until_key = $request->input('until_key');
+
+    //検索
+    $query = User::query();
+    if(!empty($name_key)){
+        $query->where('name', 'like', '%'.$name_key.'%');
+    }
+    if(!empty($id_key)){
+        $query->where('login_id', $id_key);
+    }
+    if(!empty($sex_key)){
+        $query->where('sex', $sex_key);
+    }
+    if(!empty($pref_key)){
+        $query->where('prefecture', $pref_key);
+    }
+    if(!empty($from_key) && !empty($until_key)){
+        $query->whereBetween('created_at', [$from_key, $until_key]);
+    }elseif(!empty($from_key) && empty($until_key)){
+        $query->where('created_at', '>', $from_key);
+    }elseif(empty($from_key) && !empty($until_key)){
+        $query->where('created_at', '<', $until_key);
+    }
+    $results = $query->count();
+    $users = $query->paginate(5);
+
+    //戻る用パラメータのセッション
+    $back = http_build_query($request->input());
+    session(['back' => $back]);
+
+        return view('user/index', ['users' => $users, 'name_key' => $name_key, 'id_key' => $id_key, 'sex_key' => $sex_key, 'pref_key' => $pref_key, 'from_key' => $from_key, 'until_key' => $until_key, 'results' => $results ]);
     }
 
     //登録
@@ -75,7 +110,9 @@ class UserController extends Controller
         $user->name = $request->name;
         $user->login_id = $request->login_id;
         $user->email = $request->email;
-        if(!empty($request->password)){ $user->password =$request->password; }
+        if(!empty($request->password)){
+          $user->password =$request->password;
+        }
         $user->sex = $request->sex;
         $user->zip = $request->zip;
         $user->prefecture = $request->prefecture;
