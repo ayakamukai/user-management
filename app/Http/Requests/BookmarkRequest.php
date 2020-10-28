a0d5e50c6b5c5797fc3219209f13d4b60540b649
ayaka_mukai <ayaka_mukai@bwave.co.jp>
2020-10-28 22:00:14 +0900

    [課題７]ブックマークバリデーションの追加

--- app/Http/Requests/BookmarkRequest.php
+++ app/Http/Requests/BookmarkRequest.php
@@ -24,7 +24,26 @@ class BookmarkRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'site_name'=> ['required','max:255'],
+            'url'=> ['required','max:255','url'],
         ];
     }
+
+    public function messages()
+    {
+        return [
+            'required' => ':attributeは必須項目です。入力して下さい',
+            'max' => ':max文字以内で入力して下さい',
+            'url.url' => '正しいURL形式で入力してください',
+        ];
+    }
+
+    public function attributes()
+    {
+        return [
+            'site_name' => 'サイト名',
+            'url' => 'サイトURL',
+            ];
+    }
+
 }
